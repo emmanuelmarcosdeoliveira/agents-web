14334ac3e022b56887fa25e0f3e653114a8cfeb5
Emmanuel Marcos de Oliveira <emmanuelmarcosdeoliveira@gmail.com>
2025-07-08 11:42:29 -0300

    feat: installed the tanstack query library and created the data feat for our back-end application

--- src/app.tsx
+++ src/app.tsx
@@ -1,18 +1,22 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { CreateRoom } from '@/pages/create-room'
 import Header from './components/ui/Header'
 import { Room } from './pages/room'
 import { ThemeProvider } from './theme/theme-provider'
 export function App() {
+  const queryClient = new QueryClient()
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <Header />
-      <BrowserRouter>
-        <Routes>
-          <Route element={<CreateRoom />} index />
-          <Route element={<Room />} path="/room" />
-        </Routes>
-      </BrowserRouter>
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <Header />
+        <BrowserRouter>
+          <Routes>
+            <Route element={<CreateRoom />} index />
+            <Route element={<Room />} path="/room/:roomId" />
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
+    </QueryClientProvider>
   )
 }
--- src/pages/room.tsx
+++ src/pages/room.tsx
@@ -1,7 +1,14 @@
+import { Navigate, useParams } from 'react-router-dom'
+
+type RoomParams = {
+  roomId: string
+}
+
 export function Room() {
-  return (
-    <div>
-      <h1>Room details</h1>
-    </div>
-  )
+  const params = useParams<RoomParams>()
+  if (!params.roomId) {
+    return <Navigate replace to="/" />
+  }
+
+  return <div>Room Details</div>
 }
